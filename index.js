b495bf3d3781c2aac2ceb9d6b7beefcc6e51c5f9
warathepj <80021220+warathepj@users.noreply.github.com>
2021-06-24 12:38:50 +0700

    2 02 44

--- index.js
+++ index.js
@@ -4,9 +4,18 @@
 
 const fs = require("fs");
 
-fs.readFile("Majic.js", 'utf8', (err, data) => {
+fs.readFile("test.txt", 'utf8', (err, data) => {
     if(err) throw err;
     console.log(data);
 })
 
 const data = fs.readFileSync('test.txt', {encoding:'utf-8', flag:'r'});
+console.log(data);
+
+fs.stat('test.txt', (err, stats) => {
+    if(err) {
+        console.error(err);
+        return
+    }
+    console.log(stats.isFile());
+})
