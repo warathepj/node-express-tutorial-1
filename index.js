2d26db1cd67535f29f8bde2844a0bb27a1bebffb
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-22 12:41:30 +0700

    2 39 48

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer(function(req, res) {
-    fs.readFile("test.json", (err, data) =>{
-        res.end(data);
-    })
+    const stream = fs.createReadStream("test.json");
+    stream.pipe(res);
+    // fs.readFile("test.json", (err, data) =>{
+    //     res.end(data);
+    // })
 })
 server.listen(3000, () => {console.log("port 3000")});
